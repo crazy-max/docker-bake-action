a35ee0c096f9e6446a0f8489e6b9facca5125c0e
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2026-01-07 16:30:17 +0100

    update jest config since 30.0
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
+/* eslint-disable @typescript-eslint/no-require-imports */
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
 const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'docker-bake-action-'));
 
@@ -9,9 +10,7 @@ process.env = Object.assign({}, process.env, {
   GITHUB_REPOSITORY: 'docker/bake-action',
   RUNNER_TEMP: path.join(tmpDir, 'runner-temp'),
   RUNNER_TOOL_CACHE: path.join(tmpDir, 'runner-tool-cache')
-}) as {
-  [key: string]: string;
-};
+});
 
 module.exports = {
   clearMocks: true,
